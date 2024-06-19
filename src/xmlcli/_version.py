5171ebc6b70f96d42b47346b927c3c74ff4a01e6
Prakash B R <prakash.b.r@intel.com>
2024-06-19 12:21:33 +0530

    removed the dependency of setuptool module from xmlcli

--- src/xmlcli/_version.py
+++ src/xmlcli/_version.py
@@ -1,4 +1,12 @@
-from distutils.version import LooseVersion
+class Version:
+  def __init__(self, major, minor, build, tag=""):
+    self.major=major
+    self.minor=minor
+    self.build=build
+    self.tag=tag
+
+  def __str__(self):
+    return f"{self.major}.{self.minor}.{self.build}{self.tag}"
 
 # MAJOR ----------
 # incremented any time you change the API that may break backwards compatibility
@@ -7,12 +15,10 @@ MAJOR = 2
 # MINOR ------------
 MINOR = 0
 # BUILD ------
-BUILD = 4  # or __revision__
+BUILD = 5  # or __revision__
 # TAG -------
 TAG = ""
-__version__ = LooseVersion("{major}.{minor}.{build}{tag}".format(
-  major=MAJOR,
-  minor=MINOR,
-  build=BUILD,
-  tag=TAG
-))
+
+version_instance = Version(MAJOR, MINOR, BUILD, TAG)
+
+__version__ = str(version_instance)
